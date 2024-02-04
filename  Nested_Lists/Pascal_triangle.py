b3972a316356b74dc8d933cff0ce91aa3704529a
Kasaru <artemiy064@bk.ru>
2024-02-04 11:17:16 +0300

    На вход программе подается число � n. Напишите программу, которая возвращает указанную строку треугольника Паскаля в виде списка (нумерация строк начинается с нуля).

---
+++  Nested_Lists/Pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal(n):
+    result = []
+    for i in range(0, n + 1):
+        row = [1] * (i + 1)
+        for j in range(i):
+            if j != 0 and j != i:
+                row[j] = result[i - 1][j - 1] + result[i - 1][j]
+        result.append(row)
+    return result
+
+
+n = int(input())
+result = pascal(n)
+print(result[n])
