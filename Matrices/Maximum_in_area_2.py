01903531ceb1c41e406e673f0689fa1be0cdad9e
Kasaru <artemiy064@bk.ru>
2024-02-04 11:21:53 +0300

    Напишите программу, которая выводит максимальный элемент в заштрихованной области квадратной матрицы.

---
+++ Matrices/Maximum_in_area_2.py
@@ -0,0 +1,13 @@
+n = int(input())
+matrix = []
+chars = []
+maximum = -999999
+for i in range(n):
+    matrix.append(input().split(' '))
+for i in range(n):
+    for j in range(n):
+        if (j <= i <= n - 1 - j) or (j >= i >= n - 1 - j):
+            chars.append(matrix[i][j])
+            if maximum < int(matrix[i][j]):
+                maximum = int(matrix[i][j])
+print(maximum)
