10f635a55325dd3b821f1288666f30b730d6cbd0
Kasaru <artemiy064@bk.ru>
2024-02-21 11:22:50 +0300

    Ученики 10 10 класса онлайн-школы BEEGEEK получили задание прочесть на летних каникулах три книги:
    "Что такое математика?";
    "Математическая составляющая";
    "100 гениальных идей по математике".
    Оказалось, что
    �
    n учеников прочитали первую книгу,
    �
    m учеников — вторую,
    �
    k учеников — третью. Также известно, что
    �
    x учеников прочли первую или вторую, или обе эти книги,
    �
    y учеников — вторую или третью, или обе,
    �
    z учеников — первую и третью, или хотя бы одну из этих двух книг. Полностью выполнили задание только
    �
    t учеников. Всего в
    10
    10 классе учится
    �
    a учеников. Напишите программу, которая выводит сколько учеников:
    
    прочитали только одну книгу;
    прочитали две книги;
    не прочитали ни одной из рекомендованных книг.
    Формат входных данных
    На вход программе подаются числа n,m,k,x,y,z,t,a, каждое на отдельной строке.
    
    Формат выходных данных
    Программа должна вывести три числа в соответствии с условием задачи, каждое на отдельной строке.

---
+++ Sets/Books_to_read.py
@@ -0,0 +1,4 @@
+n,m,k,x,y,z,t,a = int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input())
+print(a - t -(n + k - t - z + n + m - t - y + k + m - x - t)-(a + n + m + k - x - y - z - t))
+print(n + k - t - z + n + m - t - y + k + m - x - t)
+print(a + n + m + k - x - y - z - t)
