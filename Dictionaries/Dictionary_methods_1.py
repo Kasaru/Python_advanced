bfe33e318ddb067be1064b37d505499f36cdfd25
Kasaru <artemiy064@bk.ru>
2024-03-11 10:15:10 +0300

    Дополните приведенный код так, чтобы он объединил содержимое двух словарей dict1 и dict2: если ключ есть в обоих словарях, добавьте его в результирующий словарь со значением, равным сумме соответствующих значений из первого и второго словаря; если ключ есть только в одном из словарей, добавьте его в результирующий словарь с его текущим значением. Результирующий словарь необходимо присвоить переменной result.
    Примечание. Выводить содержимое словаря result не нужно.

---
+++ Dictionaries/Dictionary_methods_1.py
@@ -0,0 +1,9 @@
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+result = {}
+result = dict1.copy()
+result.update(dict2)
+for k,v in dict1.items():
+    for k1,v1 in dict2.items():
+        if k1 == k:
+            result[k] = v+v1
